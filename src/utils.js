bf006ae70945b0354e73ec0423af36e66ec47e85
mediapower13 <nurudeenbolaman@gmail.com>
2026-04-24 23:17:49 +0100

    finance

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,26 @@ export const formatCurrency = (value) => {
 
 
 
+require('dotenv').config(); // Load environment variables
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Access variables
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB using env variable
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Environment variables working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
